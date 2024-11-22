c13cba4248b0fcf1525dadd75ef803b0bae20368
Hyunseung Ryu <rhseungg@gmail.com>
2024-11-23 01:04:31 +0900

    Initial commit

---
+++ src/main/kotlin/com/rhseung/backpack/BackpackDataGenerator.kt
@@ -0,0 +1,9 @@
+package com.rhseung.backpack
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object BackpackDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
